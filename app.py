b7a23cfa03bdbd3b53b360d7f08cf48df1525d99
sp1rs <shashank201101193@gmail.com>
2019-09-02 03:47:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def dummy(a):
+    if a==1:
+        return 1
+    elif a==2:
+        return 2
+    elif a==3:
+        return 3
+    return 4
---
+++ test_app.py
@@ -0,0 +1,8 @@
+import unittest
+
+from app import dummy
+
+class TestDummy(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(dummy(1), 1)
