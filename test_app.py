7f44637b948483b3296b17012df98b8609dbdd6f
sp1rs <shashank201101193@gmail.com>
2019-09-02 03:58:55 +0530

    first commit

--- test_app.py
+++ test_app.py
@@ -7,3 +7,5 @@ class TestDummy(unittest.TestCase):
     def test_dummy(self):
         self.assertEqual(dummy(1), 1)
         self.assertEqual(dummy(2), 2)
+        self.assertEqual(dummy(3), 3)
+        self.assertEqual(dummy(3), 3)
